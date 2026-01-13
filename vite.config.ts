19c861bb8b51ad8ef29b7db4c1bd144b2c39d8cd
fiscalcsk-prog <fiscal.csk@gmail.com>
2026-01-13 20:25:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig(() => {
   return {
-    base: './', // 👈 ESSENCIAL PARA FUNCIONAR NO VERCEL
+    base: './',
 
     server: {
       port: 3000,
@@ -14,11 +13,6 @@ export default defineConfig(({ mode }) => {
 
     plugins: [react()],
 
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
-
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
@@ -26,4 +20,5 @@ export default defineConfig(({ mode }) => {
     }
   };
 });
+
 
